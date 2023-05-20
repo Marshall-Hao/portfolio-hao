9f332410ee9932df785be7324444b0be75ab3b23
Marshall Chan <marshallchan666@outlook.com>
2023-05-20 16:12:05 +0800

    fix: small fix

--- components/canvas/View.tsx
+++ components/canvas/View.tsx
@@ -21,27 +21,7 @@ type CProps = {
 };
 
 export const Common = ({ color }: CProps) => (
-  <Suspense
-    fallback={
-      <Box
-        m="auto"
-        mt={["-20px", "-60px", "-120px"]}
-        mb={["-40px", "-140px", "-200px"]}
-        w={[280, 480, 640]}
-        h={[280, 480, 640]}
-        position="relative"
-      >
-        <Spinner
-          size="xl"
-          position="absolute"
-          left="50%"
-          top="50%"
-          ml="calc(0px - var(--spinner-size) / 2)"
-          mt="calc(0px - var(--spinner-size))"
-        />
-      </Box>
-    }
-  >
+  <Suspense fallback={null}>
     {color && <color attach="background" args={[color]} />}
     <ambientLight intensity={0.5} />
     <pointLight
